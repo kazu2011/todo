05a71d63a212d2c7875aa5c3faf7b9f3a9959b33
kazu2011 <68805964+kazu2011@users.noreply.github.com>
2020-08-10 15:46:14 +0900

    Update app.js

--- js_practice/app.js
+++ js_practice/app.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.get('https://kazu2011.github.io/todo/js_practice/views/top.ejs',(req,res) => {
+  res.render('top.ejs');
+});
+
+app.listen('kazu2011.github.io');
+
